0b181ff2a69c8918d7613b4f7f7fa208120ccb5a
sandhya2304 <sandhyasharma0423@gmail.com>
2021-03-08 20:35:17 +0530

    sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,21 @@
 //alert("hello");
+
+// cross btn
+
+const toggleSidebar = () =>{
+	if($('.sidebar').is(":visible")){
+		//true
+		//show krna hai
+		
+		$(".sidebar").css("display","none");
+		$(".content").css("margin-left","0%");
+		
+	}else{
+		//false
+		//band krna hai
+		
+		$(".sidebar").css("display","block");
+		$(".content").css("margin-left","20%");
+		
+	}
+}

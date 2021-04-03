29fa9cced4b1289fdca15208abf2a6dfee4fa775
Sandhyua Sharma <sandhyasharma0423@gmail.com>
2021-04-03 23:32:02 +0530

    otp send to email

--- src/main/java/com/smart/controller/ForgotController.java
+++ src/main/java/com/smart/controller/ForgotController.java
@@ -4,14 +4,21 @@ import java.util.Random;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.service.EmailService;
+
 @Controller
 public class ForgotController
 {
+	
+	@Autowired
+	EmailService emailService;
+	
 	
 	Random rand  = new Random(1000);
 	
@@ -29,7 +36,7 @@ public class ForgotController
 		System.out.println("email .."+email);
 		
 		
-		//generationg otp 4 digit
+		//Generating otp 4 digit
 		
 		
 		int otp = rand.nextInt(9999999);
@@ -38,13 +45,25 @@ public class ForgotController
 		
 		//write code for send otp to email
 		
+		String subject = "OTP from MAnager";
+		String message  = "OTP is = "+otp;
+		String to = email;
 		
+		boolean flag = this.emailService.sendEmail(subject, message, to);
 		
+		if(flag)
+		{
+			session.setAttribute("otp", otp);
+			return "verifyotp";
+			
+		}else{
+			
+			session.setAttribute("msg","checked your email ID");
+			
+			return "forgotemailForm";
+		}
+
 		
-		
-		
-		
-		return "verifyotp";
 	}
 
 }
---
+++ src/main/java/com/smart/service/EmailService.java
@@ -0,0 +1,126 @@
+package com.smart.service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
+import java.util.Properties;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService
+{
+	
+	public boolean sendEmail(String subject,String message,String to)
+    {
+
+        boolean status= false;
+
+        String from = "tehie9762@gmail.com";
+
+        //Variable for gmail
+        String host = "smtp.gmail.com";
+
+        //get the system properties
+        Properties props = System.getProperties();
+        System.out.println(props);
+
+        //setting important function to properties object
+
+
+        //set host
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.ssl.enable","true");
+        props.put("mail.smtp.auth", "true");
+
+
+        //step 1. get the session object
+
+        Session session = Session.getInstance(props,new Authenticator() {
+
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("techie9762@gmail.com","");
+            }
+
+        });
+
+        session.setDebug(true);
+
+        //step 2. compose the msg
+        MimeMessage mime = new MimeMessage(session);
+
+
+        try{
+
+            mime.setFrom(from);
+
+            //adding receipt to msg
+            mime.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            //adding subject to msg
+            mime.setSubject(subject);
+
+
+            //set attachment
+            //file path
+            String path = "E:\\img\\hadoop.jpg";
+
+            MimeMultipart mim = new MimeMultipart();
+            //text
+
+            //file
+
+            MimeBodyPart text = new MimeBodyPart();
+
+
+
+            MimeBodyPart file = new MimeBodyPart();
+
+            try
+            {
+
+                text.setText(message);
+
+                File ff= new File(path);
+                file.attachFile(ff);
+
+
+                mim.addBodyPart(text);
+                mim.addBodyPart(file);
+
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+
+
+
+            mime.setContent(mim);
+
+
+            //Step 3. send the message
+            Transport.send(mime);
+
+
+
+            System.out.println("Sent succesfuly");
+
+            status = true;
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+       return  status;
+    }
+	
+
+}
